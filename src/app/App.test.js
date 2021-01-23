c528be26dabb31a86b4ae795756d741a518d9740
natebowang <120029790@qq.com>
2021-01-23 15:26:17 +1300

    Create test for updateWiw reducer.

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -4,12 +4,21 @@ import { Provider } from 'react-redux';
 import store from '../store/store';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+test('renders App and resize window inner width, dispatch updateWiw. ', () => {
+    render(
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+    // resize window to 500px, and expect app.wiw equals to 500
+    window.innerWidth=500;
+    window.dispatchEvent(new Event('resize'));
+    expect(store.getState().app.wiw).toBe(500);
+
+    // resize window to 1024px, and expect app.wiw equals to 1024
+    window.innerWidth=1024;
+    window.dispatchEvent(new Event('resize'));
+    expect(store.getState().app.wiw).toBe(1024);
 });
+
